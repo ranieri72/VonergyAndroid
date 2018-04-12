4d4e0fcac79c9507a8a2711405548f5ba3908cb1
pdsfilho <p.dantas@outlook.com>
2018-04-12 01:37:18 -0300

    1º Commit

---
+++ app/src/main/java/com/vonergy/view/ProfileActivity.java
@@ -0,0 +1,15 @@
+package com.vonergy.view;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.vonergy.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
